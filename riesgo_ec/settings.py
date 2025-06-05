87cfab04e8ab2303c685d69cb027381a80484ea5
Francisco Arias <fariasp2@unemi.edu.ec>
2025-06-04 23:26:05 -0500

    Arreglo del leak

--- riesgo_ec/settings.py
+++ riesgo_ec/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -150,5 +151,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "franciscoarias108@gmail.com"  # tu correo Gmail
-EMAIL_HOST_PASSWORD = "wyvw qedk zjmd pjbh"
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
